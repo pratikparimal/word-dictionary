fee5d212898b70056222c28f0bad516fa7bcd2c5
pratikparimal <pratik.parimal@gmail.com>
2019-09-01 15:39:25 +0530

    first commit

---
+++ src/main/java/com/happiestminds/challenge/worddictionary/WordDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.happiestminds.challenge.worddictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happiestminds/challenge/worddictionary/WordDictionaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.happiestminds.challenge.worddictionary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordDictionaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
